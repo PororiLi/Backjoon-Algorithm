f67b0d5a779f5fce4f6c3b66651ae7ed11b1e9ad
pororili <xiozioll@gmail.com>
2021-01-22 22:41:19 +0900

    109__ Series

---
+++ inputoutput/quefive.java
@@ -0,0 +1,17 @@
+// import java.util.Scanner;
+
+// public class quefive {
+//     /*10950두정수 AB입력 받고 합출력 
+//     첫째줄에 테스트 ㄱ케이스 개수가 있고 각줄에 에이비 주어짐.
+//     테스트 케이스 마다 합 출력 */
+//     public static void main(String argvs[]){
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         while(num > 0){
+//             int A = sc.nextInt();
+//             int B = sc.nextInt();
+//             System.out.println(A+B);
+//             num--;
+//         }
+//     }
+// }
---
+++ inputoutput/quefiveone.java
@@ -0,0 +1,13 @@
+// import java.util.Scanner;
+
+// public class quefiveone {
+//     /*10951 */
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         while(sc.hasNext()){//다음 입력이 있을때까지 동작 시키려면 scanner.hasNext() 함수를 사용하면 됨.
+//             int A = sc.nextInt();
+//             int B = sc.nextInt();
+//             System.out.println(A+B);
+//         }
+//     }
+// }
