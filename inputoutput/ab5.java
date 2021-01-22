89902dd07314491d80ddcc497b3cfdb45ffd60c9
pororili <xiozioll@gmail.com>
2021-01-23 01:24:08 +0900

    last 109 series

--- inputoutput/ab5.java
+++ inputoutput/ab5.java
@@ -1,17 +1,17 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class ab5 {
-    public static void main(String argv[]){
-        int a, b;
-        Scanner scan = new Scanner(System.in);
-        while(scan.hasNext()){
-            a = scan.nextInt();
-            b = scan.nextInt();
-            if(a==0 && b==0){
-                System.exit(0);
-            }else{
-                System.out.println(a+b);
-            }
-        }
-    }
-}
+// public class ab5 {
+//     public static void main(String argv[]){
+//         int a, b;
+//         Scanner scan = new Scanner(System.in);
+//         while(scan.hasNext()){
+//             a = scan.nextInt();
+//             b = scan.nextInt();
+//             if(a==0 && b==0){
+//                 System.exit(0);
+//             }else{
+//                 System.out.println(a+b);
+//             }
+//         }
+//     }
+// }
