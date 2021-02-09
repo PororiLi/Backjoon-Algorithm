f920f3bed5f72fc2b001dddd6ef9c888a036cdb9
pororili <xiozioll@gmail.com>
2021-02-09 18:57:00 +0900

    210208

--- inputoutput/Main_11721.java
+++ inputoutput/Main_11721.java
@@ -1,22 +1,22 @@
-import java.util.Scanner;
-/**
- * Main_11721
- */
-public class Main_11721 {
-    public static void main(String[] args) {
-        String str;
+// import java.util.Scanner;
+// /**
+//  * Main_11721
+//  */
+// public class Main_11721 {
+//     public static void main(String[] args) {
+//         String str;
 
-        java.util.Scanner sc = new Scanner(System.in);
-        str = sc.next();
+//         java.util.Scanner sc = new Scanner(System.in);
+//         str = sc.next();
         
-        if(str.length() < 100){
-            for(int i = 0; i < str.length(); i++){
-                System.out.print(str.charAt(i));
+//         if(str.length() < 100){
+//             for(int i = 0; i < str.length(); i++){
+//                 System.out.print(str.charAt(i));
 
-                if(i %10 == 9){
-                    System.out.println();
-                }
-            }   
-        }
-    }
-}
+//                 if(i %10 == 9){
+//                     System.out.println();
+//                 }
+//             }   
+//         }
+//     }
+// }
