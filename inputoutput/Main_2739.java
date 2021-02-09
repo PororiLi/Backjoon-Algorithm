d49e9e495febc1aa0aa0efec0fb62474c5424e4c
pororili <xiozioll@gmail.com>
2021-02-09 21:07:31 +0900

    Solved at 210209

---
+++ inputoutput/Main_2739.java
@@ -0,0 +1,23 @@
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.util.StringTokenizer;
+// import java.io.InputStreamReader;
+
+// public class Main_2739{
+//     public static void main(String argvs[]){
+//         try{
+//             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//             StringTokenizer st = new StringTokenizer(br.readLine());
+//             int n = Integer.parseInt(st.nextToken());
+            
+//             for(int i = 1; i<10 ; i++)
+//             {
+//                 System.out.println(n+" * "+i+" = "+(n*i));
+
+//             }
+
+//         } catch(IOException e){
+//             e.printStackTrace();
+//         }
+//     }
+// }
--- inputoutput/Main_2742.java
+++ inputoutput/Main_2742.java
@@ -1,30 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.StringTokenizer;
 
-public class Main_2742 {
-    // public static void main(String[] args) throws IOException{//BufferedReader는 예외처리 기본
-    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //     StringTokenizer st = new StringTokenizer(br.readLine());
-    //     int n = Integer.parseInt(st.nextToken());
+// public class Main_2742 {
+//     // public static void main(String[] args) throws IOException{//BufferedReader는 예외처리 기본
+//     //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     //     StringTokenizer st = new StringTokenizer(br.readLine());
+//     //     int n = Integer.parseInt(st.nextToken());
         
-    //     for(int i = 0; i < n; i++){
-    //         System.out.println(n-i);
-    //     }
-    // }
+//     //     for(int i = 0; i < n; i++){
+//     //         System.out.println(n-i);
+//     //     }
+//     // }
 
-    public static void main(String[] args) {// BufferedReader는 예외처리 기본
-        try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
+//     public static void main(String[] args) {// BufferedReader는 예외처리 기본
+//         try{
+//             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//             StringTokenizer st = new StringTokenizer(br.readLine());
+//             int n = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < n; i++) {
-                System.out.println(n - i);
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-}
+//             for (int i = 0; i < n; i++) {
+//                 System.out.println(n - i);
+//             }
+//         } catch (IOException e){
+//             e.printStackTrace();
+//         }
+//     }
+// }
