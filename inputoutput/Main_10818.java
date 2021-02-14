63ce3eaa06d42ff8d6c46dd13ff8183746a7cd4d
pororili <xiozioll@gmail.com>
2021-02-14 14:00:22 +0900

    solved 210214

--- inputoutput/Main_10818.java
+++ inputoutput/Main_10818.java
@@ -2,45 +2,57 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main_10818 {
-    public static void main (String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int[] n_array = new int[n];
-        br.readLine();
-
-
-        for (int i = 0; i<n; i++) {
-            n_array[i] = Integer.parseInt(st.nextToken());//여기가 문제
-        }
 
         int min = 0;
         int max = 0;
-        for (int i = 0; i<n; i++) {
-            if(i == 0){
-                if (n_array[i] < n_array[i + 1]) {
-                    max = n_array[i+1];
-                    min = n_array[i];
-                }else{
-                    max = n_array[i];
-                    min = n_array[i+1];
-                }
-            } else {
-                if (n_array[i] < min){
-                    min = n_array[i];
-                } else if(n_array[i] > max){
-                    max = n_array[i];
+
+        if(n > 1){
+
+            int[] nArray = new int[n];
+            String strs[] = br.readLine().split(" ");
+            
+            for(int i = 0; i<n; i++){
+                nArray[i] = Integer.parseInt(strs[i]);
+            }
+
+      
+            
+            for (int i = 0; i < n; i++) {
+               
+               if (i == 0 ) {
+                    if (nArray[i]< nArray[i + 1]) {
+                        max = nArray[i + 1];
+                        min = nArray[i];
+                    } else {
+                        max = nArray[i];
+                        min = nArray[i + 1];
+                    }
+                } else {
+                    if (nArray[i] < min) {
+                        min = nArray[i];
+                    } else if (nArray[i] > max) {
+                        max = nArray[i];
+                    }
                 }
             }
+
+            bw.write(min + " " + max);
+
+            br.close();
+            bw.flush();
+            bw.close();
+        }else if (n == 1){
+            min = Integer.parseInt(br.readLine());
+            max = min;
+            System.out.println(min+"\s"+max);
         }
-
-        bw.write(min+" "+max);
-        
-        br.close();
-        bw.flush();
-        bw.close();
-
-    }
+    } 
 }
+
+// \s 공백 에러 나는 경우도 있음. 1인 경우의 반례를 생각해주지 못함.
