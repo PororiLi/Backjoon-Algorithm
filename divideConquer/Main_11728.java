7de13dbd960245955f9b0720ef992fbf93909c82
pororili <xiozioll@gmail.com>
2021-03-03 18:36:28 +0900

    210303

--- divideConquer/Main_11728.java
+++ divideConquer/Main_11728.java
@@ -1,18 +1,28 @@
 package divideConquer;
 
 import java.io.*;
+import java.util.*;
 
 public class Main_11728 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] in = br.readLine().split(" ");
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
-        int sizeA = Integer.parseInt(in[0]);
-        int sizeB = Integer.parseInt(in[1]);
-        int[] arrA[] = new int[sizeA];
-        int[] arrA[] = new Int[sizeB];
-
+        int sizeA = sc.nextInt();
+        int sizeB = sc.nextInt();
+        // int[] arrAB = new int[sizeA+sizeB]; //얘가 더 빠름.
+        ArrayList<Integer> abList = new ArrayList<Integer>();
         
-             
+        for(int i = 0; i<sizeA+sizeB; i++) {
+            // arrAB[i] = sc.nextInt();
+            abList.add(i, sc.nextInt());
+        }
+        // Arrays.sort(arrAB);
+        Collections.sort(abList);
+        for(int i = 0; i<sizeA+sizeB; i++) {
+            // sb.append(arrAB[i]).append(" ");
+            sb.append(abList.get(i)).append(" ");
+        }
+        System.out.println(sb);
     }
 }
