1b3391bb644d0c9a1040558d14ad69da27551067
PororiLi <46364839+PororiLi@users.noreply.github.com>
2021-06-26 16:03:56 +0900

    Update Main_ant.java

--- DP/Main_ant.java
+++ DP/Main_ant.java
@@ -1,25 +1,25 @@
-// package DP;
+ package DP;
 
-// import java.io.*;
-// import java.util.StringTokenizer;
+ import java.io.*;
+ import java.util.StringTokenizer;
 
 
-// public class Main_ant {
-//     public static void main(String[] args) throws IOException {
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ public class Main_ant {
+     public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-//         int n = Integer.parseInt(br.readLine());
-//         StringTokenizer st = new StringTokenizer(br.readLine());
-//         int kArray[] = new int[n+1];
+         int n = Integer.parseInt(br.readLine());
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         int kArray[] = new int[n+1];
 
-//         for(int i = 1; i<=n; i++) {
-//             kArray[i] = Integer.parseInt(st.nextToken(" "));
-//         }
-//         kArray[0] = 0;
+         for(int i = 1; i<=n; i++) {
+             kArray[i] = Integer.parseInt(st.nextToken(" "));
+         }
+         kArray[0] = 0;
 
-//         for(int i = 2; i<n+1; i++) {
-//             kArray[i] += kArray[i-2];
-//         }
-//         System.out.println(Math.max(kArray[n], kArray[n - 1]));
-//     }
-// }
+         for(int i = 2; i<n+1; i++) {
+             kArray[i] += kArray[i-2];
+         }
+         System.out.println(Math.max(kArray[n], kArray[n - 1]));
+     }
+ }
